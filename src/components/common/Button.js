b0b6800ec21c801ccd5f6a431bb72f65b29f8f13
Alexrocchi <alexrocchi.a@gmail.com>
2023-06-03 15:46:26 +0200

    Bulk styles using the styled-components css helper

--- src/components/common/Button.js
+++ src/components/common/Button.js
@@ -1,14 +1,21 @@
-import styled from 'styled-components';
+import styled, {css} from 'styled-components';
 
 /* Button -> Remember that all React Components have to start with a Capital letter */
 const Button = styled.button`
     color: white;
     background-color: ${(props) => props.secondary ? 'black' : '#f8049c'};
     font-weight: bold;
-    padding: 8px;
-    border-radius: 4px;
+    ${(props) => props.large ? css`
+        padding: 10px;
+        font-size: 1.5em;
+        border-radius: 5px;
+    ` : css`
+        padding: 8px;
+        font-size: 1em;
+        border-radius: 4px;
+    
+    `}
     box-shadow: none;
-    font-size: 1em;
     border: none;
     width: 100%;
     display: block;
