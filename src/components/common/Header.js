65abd50cc34e506e854ca021dfa7aaf820cee411
Alexrocchi <alexrocchi.a@gmail.com>
2023-06-04 02:10:28 +0200

    useState React hook with menu icon + event handlers

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from 'react';
 import styled from "styled-components";
 import { Link as ReactRouterDomLink, useLocation } from "react-router-dom";
 
@@ -15,7 +15,7 @@ const HeaderWrapper = styled.header`
 `;
 
 const Menu = styled.nav`
-    display: block;
+    display: ${p => p.open ? 'block' : 'none'};
     font-family: 'Open Sans';
     position: absolute;
     width: 100%;
@@ -62,11 +62,34 @@ const StyledLink = styled(Link)`
     color: #000;
 `;
 
+const MobileMenuIcon = styled.div`
+    margin: auto 0 auto auto;
+    width: 25px;
+    min-width: 25px;
+    padding: 5px;
+    > div{
+        height: 3px;
+        background-color: #000;
+        margin: 5px 0;
+        width: 100%;
+    }
+
+    @media(width >= 768px){
+        display: none;
+    }
+`;
+
 export function Header() {
     const {pathname} = useLocation();
+    const [menuOpen, setMenuOpen] = useState(false);
     return (
         <HeaderWrapper>
-            <Menu>
+            <MobileMenuIcon onClick={() => setMenuOpen(s => !s)}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </MobileMenuIcon>
+            <Menu open={menuOpen}>
                 <StyledLink to={"/"} isActive={pathname === '/'}>Home</StyledLink>
                 <StyledLink to={"/login"} isActive={pathname === '/login'}>Login</StyledLink>
             </Menu>
